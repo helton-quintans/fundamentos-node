26443acdb3357437fd67bd3ec518fae301691658
helton-quintans <helton.quit@gmail.com>
2024-01-13 18:51:57 -0300

    cria e retorna user []
    Signed-off-by: helton-quintans <helton.quit@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+
+    if(method === 'GET' && url === '/users') {
+        return res.end(JSON.stringify(users))
+    }
+
+    if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'jon',
+            email: 'jonhdoe@email.com'
+        })
+        return res.end('Criação de usuário')
+    }
+
+    console.log(method, url)
     return res.end('Hello carai')
 })
 
